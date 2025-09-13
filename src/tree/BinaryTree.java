0926435daae5dda5804f454fba4b174e893a02aa
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2025-09-12 21:48:31 -0300

    t

---
+++ src/tree/BinaryTree.java
@@ -0,0 +1,28 @@
+package tree;
+
+public class BinaryTree {
+    private Node root;
+
+    public void add(int value){
+        root = add(root, value);
+    }
+
+    private Node add(Node node, int value){
+        if(node == null){
+            node = new Node(value);
+        }else{
+            if(value < node.value){
+                node.left = add(node.left, value);
+            }
+        }
+        return node;
+    }
+    private static class Node{
+        int value;
+        Node left;
+        Node right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+}
